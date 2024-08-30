962549ac1854f73cc6e1e4401fe8894886c77816
Savindu <savinduruhunuhewa@gmail.com>
2024-08-30 05:57:55 +0530

    Create the MVC architecture  and product class

--- src/main/java/com/savindu/kfcsrilanka/model/Product.java
+++ src/main/java/com/savindu/kfcsrilanka/model/Product.java
@@ -2,8 +2,44 @@ package com.savindu.kfcsrilanka.model;
 
 public class Product {
 
-	
+private int productCode;
+private String productName;
+private double productPrice;
+
 public Product() {
+
+}
+
+public Product(int productCode, String productName, double productPrice ) {
+	this.productCode=productCode;
+	this.productName=productName;
+	this.productPrice=productPrice;
+	
 	
 }
+
+public int getProductCode() {
+	return productCode;
+}
+
+public void setProductCode(int productCode) {
+	this.productCode = productCode;
+}
+
+public String getProductName() {
+	return productName;
+}
+
+public void setProductName(String productName) {
+	this.productName = productName;
+}
+
+public double getProductPrice() {
+	return productPrice;
+}
+
+public void setProductPrice(double productPrice) {
+	this.productPrice = productPrice;
+}
+
 }
