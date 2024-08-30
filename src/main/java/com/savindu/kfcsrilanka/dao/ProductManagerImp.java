6108103c2cc85b5b984581e23050ddee319385dd
Savindu <savinduruhunuhewa@gmail.com>
2024-08-30 07:09:38 +0530

    Implement the interface

---
+++ src/main/java/com/savindu/kfcsrilanka/dao/ProductManagerImp.java
@@ -0,0 +1,39 @@
+package com.savindu.kfcsrilanka.dao;
+
+import java.util.List;
+
+import com.savindu.kfcsrilanka.model.Product;
+
+public class ProductManagerImp implements ProductManager {
+
+	@Override
+	public boolean addProduct(Product product) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean editProduct(Product product) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteProduct(int productCode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Product fetchSingleProduct(int productCode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Product> fetchAllProduct() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
