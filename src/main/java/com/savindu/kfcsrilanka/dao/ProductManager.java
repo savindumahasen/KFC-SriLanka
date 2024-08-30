9e23b30073bacb105840a5f65d48ebce541a0f0c
Savindu <savinduruhunuhewa@gmail.com>
2024-08-30 06:17:10 +0530

    Create the ProductManagerInerface

--- src/main/java/com/savindu/kfcsrilanka/dao/ProductManager.java
+++ src/main/java/com/savindu/kfcsrilanka/dao/ProductManager.java
@@ -1,5 +1,15 @@
 package com.savindu.kfcsrilanka.dao;
 
+import java.util.List;
+
+import com.savindu.kfcsrilanka.model.Product;
+
 public interface ProductManager {
+	
+	public boolean addProduct(Product product);
+	public boolean editProduct(Product product);
+	public boolean deleteProduct(int productCode);
+	public Product fetchSingleProduct(int productCode);
+	public List<Product> fetchAllProduct();
 
 }
