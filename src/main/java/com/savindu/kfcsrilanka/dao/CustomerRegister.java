48dd1d2b7512e25b147efc616025e7d1f0327101
Savindu <savinduruhunuhewa@gmail.com>
2024-09-16 12:00:06 +0530

    Create the CustomerRegister Interface

---
+++ src/main/java/com/savindu/kfcsrilanka/dao/CustomerRegister.java
@@ -0,0 +1,11 @@
+package com.savindu.kfcsrilanka.dao;
+
+import java.sql.SQLException;
+
+import com.savindu.kfcsrilanka.model.Customer;
+
+public interface CustomerRegister {
+	
+	public boolean customerRegistration(Customer customer) throws SQLException;
+
+}
