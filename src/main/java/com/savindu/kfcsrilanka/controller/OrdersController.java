f6d0b6ba1fa6570d2ca41c480b6a3b93ccbe9230
Savindu <savinduruhunuhewa@gmail.com>
2024-09-17 08:33:27 +0530

    Implement the OrdersController

---
+++ src/main/java/com/savindu/kfcsrilanka/controller/OrdersController.java
@@ -0,0 +1,50 @@
+package com.savindu.kfcsrilanka.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class OrdersController
+ */
+public class OrdersController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    String message = "";
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		clearMessage();
+		String action_type =request.getParameter("action_type");
+		if(action_type.equals("order")) {
+			RequestDispatcher rd= request.getRequestDispatcher("Orders.jsp");
+			rd.forward(request, response);
+		}else if(action_type.equals("order_now")){
+			String accountNumber = request.getParameter("accountnumber");
+			String confirmAccountNumber = request.getParameter("confirmaccountnumber");
+			if(accountNumber.equals(confirmAccountNumber)) {
+				message = "Order is successfully confirmed!";
+			    request.setAttribute("feedbackmessage",message);
+			    RequestDispatcher rd=request.getRequestDispatcher("Orders.jsp");
+			    rd.forward(request, response);
+			}else {
+				message = "Please check you Account ID and Confirm Account ID";
+			    request.setAttribute("feedbackmessage",message);
+			    RequestDispatcher rd=request.getRequestDispatcher("Orders.jsp");
+			    rd.forward(request, response);
+			}
+			}
+			
+		}
+	 private void clearMessage() {
+		 message="";
+	 }
+
+}
